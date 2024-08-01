87ce09cf493563acb652006c0f13af9761fef859
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-08-01 23:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+window.onload = function() {
+    function getDomLevel(elementId) {
+        let element = document.getElementById(elementId);
+        let level = 0;
+
+        while (element) {
+            level++;
+            element = element.parentElement;
+        }
+
+        return level;
+    }
+
+    const elementId = 'level';
+    const domLevel = getDomLevel(elementId);
+    alert(`The level of the element is: ${domLevel}`);
+};
